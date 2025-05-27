31bedb1dda492d2a0d70e3e43bab0c1cc810cef3
Matheus Avellar <MatheusAvellar@users.noreply.github.com>
2025-05-27 15:45:25 -0300

    feat: chunked export

--- README.md
+++ README.md
@@ -3,27 +3,35 @@ A Docker-based converter/exporter of tables in a .GDB file to CSV.
 
 The GDB files I had were from Firebird 1.5.6. If you have a different version, this might not work. Good luck.
 
+![Terminal window displaying the program exporting multiple tables, some with over 10 thousand rows, to CSVs.](/screenshots/1.png)
+
 
 ## Why
 I needed one and every single other alternative I tried (and I tried many!) presented me with issues. Yes this one also requires a running Firebird client, but that's what the Docker is for! You shouldn't have to worry about installing it on your precious machine.
 
 
 ## Disclaimers
-This worked for me on Windows 11 using [WSL](https://learn.microsoft.com/en-us/windows/wsl/install). If it doesn't work for you, I'm sorry, I can't help you. I wish you good luck, and hope this project can save you some hours of research.
+This worked for me on Windows 11 using [WSL](https://learn.microsoft.com/en-us/windows/wsl/install). If it doesn't work for you, sorry, I can't help you. This project uses [`firebirdsql`](https://github.com/nakagami/pyfirebirdsql/) v0.9.12 (Nov.2015) to connect to the database – meaning neither I, who didn't implement the library, nor the maintainer, who hasn't seen that code in 10 years, can really help you troubleshoot anything. I wish you good luck, and hope this project can save you some hours of research.
+
+For future's sake, I did have trouble using `ALTER TABLE` through the library; but in the end I didn't really need it, so I gave it up. I consider this project a read-only interaction with the file.
 
 The Firebird distribution used – `FirebirdSS-1.5.6.5026-0.nptl.i686.tar.gz` –
-was obtained via [SourceForge](https://sourceforge.net/projects/firebird/files/firebird-linux-i386/1.5.6-Release/) under the [Mozilla Public License v1.1](https://www.mozilla.org/en-US/MPL/1.1/). It could be downloaded on the fly during the building stage of the Dockerfile, but we're not leaving anything to chance over here.
+was obtained via [SourceForge](https://sourceforge.net/projects/firebird/files/firebird-linux-i386/1.5.6-Release/) under the [Mozilla Public License v1.1](https://www.mozilla.org/en-US/MPL/1.1/). It could be downloaded on the fly during the building stage of the Dockerfile (see comments there), but we're not leaving anything to chance over here.
 
 
 ## How to use
-In the root folder, run:
+### Setup
+When you first download the project, you must build the Docker image. In the
+root folder, run:
 ```sh
 docker build -t gdb2csv .
 ```
 
-Don't forget the trailing `.`! This will build the Docker image.
+Don't forget the trailing `.`!
 
-Place your GDB file inside the `data/` directory. Then, to export, run:
+### Use
+Then, whenever you want to export the tables, place your GDB file inside the
+`data/` directory and run:
 ```sh
 docker run --rm \
   -v ./data:/data:rw \
@@ -35,11 +43,17 @@ docker run --rm \
 ```
 
 If that's too much to remember, there's a convenient `./run.sh` you can run that
-does that.
+does that. Remember to edit in your file and table names!
 
 This will execute the `data/convert.py` file, and should output the listed tables as CSVs inside the `data/` directory. The environment variable `TABLE_LIST` receives a list of semicolon (`;`) separated table names to export.
 
 Alternatively, you can use `TABLE_LIST=all` to convert every table found in the database to CSVs.
+
+By default, the script will export the tables in chunks of 10,000 rows – this is to prevent attempting to load the results of tables with hundreds of thousands of rows into memory with `SELECT *`. If, for any reason, you do not want a chunked export, set the environment variable `NO_CHUNKS`. I didn't really test it, but it should work :)
+
+Note: you do not need to rebuild the Docker image if/when you edit the
+`convert.py` file. The `data/` directory is shared as a volume with the Docker
+image, so modifications from either you or the image are seen by both.
 
 
 ## This sucks
--- data/export.py
+++ data/export.py
@@ -14,17 +14,16 @@ def log(msg):
 	print(f"{current_time}| {msg}", flush=True)
 
 
-def get_cursor(db_path, user="SYSDBA", password="masterkey", charset="WIN1252"):
+def get_connection(db_path, user="SYSDBA", password="masterkey", charset="WIN1252"):
 	con = None
-	cur = None
-
 	# Sometimes we can't connect the first or second times we try,
 	# so let's loop it and limit our attempts
-	attempts = 10
+	MAX_ATTEMPTS = 20
+	attempt = 1
 	success = False
 	while not success:
 		# If we've tried too many times, then give up :\
-		if attempts <= 0:
+		if attempt > MAX_ATTEMPTS:
 			log("Conection to Firebird failed :(")
 			exit(1)
 		# Make sure Firebird is running. This command might be run multiple times
@@ -36,32 +35,32 @@ def get_cursor(db_path, user="SYSDBA", password="masterkey", charset="WIN1252"):
 
 		# Attempts to connect to Firebird
 		try:
-			log("Attempting connection...")
+			log(f"Attempting connection... {attempt}/{MAX_ATTEMPTS}")
 			con = firebirdsql.connect(
 				dsn=db_path,
 				user=user,
 				password=password,
 				charset=charset
 			)
-			log("Conected! Creating cursor...")
-			cur = con.cursor()
+			log("Conected!")
 			success = True
 		except firebirdsql.OperationalError as e:
 			# Uh oh, we couldn't connect. Don't fret, this happens. We just gotta try
 			# again a couple more times to make sure
 			log(f"firebirdsql.OperationalError: {e}")
-			attempts -= 1
+			attempt += 1
 		except Exception as e:
 			# Something unexpected happened. I don't know what it is. Good luck though
 			log(f"Unexpected Exception!")
 			log(repr(e))
-			attempts -= 1
+			attempt += 1
 
-	return (con, cur)
+	return con
 
 
-def execute_query(con, cur, query):
+def execute_query(con, query):
 	try:
+		cur = con.cursor()
 		log(f"Running query:\n{query}")
 		cur.execute(query)
 
@@ -82,6 +81,106 @@ def execute_query(con, cur, query):
 		exit(1)
 
 
+def execute_query(con, query):
+	try:
+		cur = con.cursor()
+		log(f"Running query:\n{query}")
+		cur.execute(query)
+
+		log("Obtaining results...")
+		START_TIME = time.time()
+		rows = cur.fetchall()
+		TOTAL_TIME = time.time() - START_TIME
+		log(f"Took {TOTAL_TIME:.1f}s")
+
+		columns = [ desc[0] for desc in cur.description ]
+		return (rows, columns)
+
+	except Exception as e:
+		log(f"Unexpected Exception!")
+		log(repr(e))
+		cur.close()
+		con.close()
+		exit(1)
+
+
+def export_table_to_csv(con, table_name):
+	log(f"Reading entire table '{table_name}'")
+
+	try:
+		query = f"""
+SELECT * FROM {table_name}
+		"""
+		(rows, columns) = execute_query(con, query)
+		df = pd.DataFrame(rows, columns=columns)
+		row_count = len(df)
+		log(f"Fetched {row_count} rows")
+
+		# Guarantees /csv directory exists
+		file_path = f"/data/csv/{table_name}.csv"
+		os.makedirs(os.path.dirname(file_path), exist_ok=True)
+		df.to_csv(file_path, mode='w', header=True, index=False)
+		log(f"Saved to '{file_path}")
+
+	except Exception as e:
+		log(f"Unexpected Exception!")
+		log(repr(e))
+		con.close()
+		exit(1)
+
+
+def export_table_to_csv_chunked(con, table_name, chunk_size):
+	log(f"Reading table '{table_name}' in chunks of {chunk_size} rows")
+
+	offset = 0
+	first_write = True
+	while True:
+		try:
+			# Get chunked results via FIRST N SKIP M syntax using RDB$DB_KEY as a
+			# unique representation of each table record
+			# [Ref FIRST/SKIP] https://www.firebirdsql.org/refdocs/langrefupd20-select.html#langrefupd20-first-skip
+			# [Ref RDB$DB_KEY] https://www.ibphoenix.com/articles/art-00000384
+			query = f"""
+SELECT FIRST {chunk_size} SKIP {offset} *
+FROM {table_name}
+ORDER BY RDB$DB_KEY
+			"""
+			(rows, columns) = execute_query(con, query)
+
+			# We could manually write the CSV but we can just use Pandas instead
+			df = pd.DataFrame(rows, columns=columns)
+			row_count = len(df)
+			log(f"Fetched {row_count} rows")
+
+			file_path = f"/data/csv/{table_name}.csv"
+			if first_write:
+				# Guarantees /csv directory exists
+				os.makedirs(os.path.dirname(file_path), exist_ok=True)
+				df.to_csv(file_path, mode='w', header=True, index=False)
+				first_write = False
+			else:
+				df.to_csv(file_path, mode='a', header=False, index=False)
+			log(f"Saved to '{file_path}")
+
+			# If we fetched no rows (empty table, row count is exact multiple
+			# of chunk_size, ...), we're done
+			if row_count <= 0:
+				log("Fetched no rows; assuming end of table")
+				break
+			# If the number of rows fetched is less than chunk_size, we're done
+			if row_count < chunk_size:
+				log(f"Fetched fewer rows than `chunk_size` ({chunk_size}); assuming end of table")
+				break
+			# Increment offset for the next chunk
+			offset += chunk_size
+
+		except Exception as e:
+			log(f"Unexpected Exception!")
+			log(repr(e))
+			con.close()
+			exit(1)
+
+
 ################################################################################
 
 
@@ -91,17 +190,19 @@ def main():
 		log(f"FB_GDB_PATH='{PATH}' is not a file!")
 		exit(1)
 
-	USER = os.environ.get("FB_USER")
-	PASS = os.environ.get("FB_PASSWORD")
+	USER = os.environ.get("FB_USER", "SYSDBA")
+	PASS = os.environ.get("FB_PASSWORD", "masterkey")
 	CHAR = os.environ.get("FB_CHARSET", "WIN1252")
 
 	TABLE_LIST = os.environ.get("TABLE_LIST", "all")
+	NO_CHUNKS = os.environ.get("NO_CHUNKS") is not None
 
 	# Attempts connection
-	(con, cur) = get_cursor(PATH)
+	con = get_connection(PATH)
 
 	# Gets all available tables in the Database
-	(found_tables, _) = execute_query(con, cur, """
+	# [Ref] https://ib-aid.com/download/docs/firebird-language-reference-2.5/fblangref-appx04-relations.html
+	(found_tables, _) = execute_query(con, """
 SELECT RDB$RELATION_NAME
 FROM RDB$RELATIONS
 WHERE RDB$SYSTEM_FLAG = 0 AND RDB$VIEW_BLR IS NULL
@@ -130,21 +231,21 @@ ORDER BY RDB$RELATION_NAME
 
 	log(f"Found {len(tables_that_exist)} requested tables (out of {len(wanted_tables)} requested, {len(found_tables)} total)\n")
 
+	CHUNK_SIZE = 10_000
 	# For every table that exists
 	for i, table in enumerate(tables_that_exist):
 		log(f"Reading table {i+1}/{len(tables_that_exist)}")
-		# Select everything
-		(rows, columns) = execute_query(con, cur, f"SELECT * FROM {table}")
 
-		# We could manually write the CSV but we can just use Pandas instead
-		log("Converting to DataFrame...")
-		df = pd.DataFrame(rows, columns=columns)
+		# If user doesn't want chunks, we just try exporting the entire table
+		if NO_CHUNKS:
+			export_table_to_csv(con, table)
+		# Otherwise, we do the more labor-intensive process of chunking the results
+		else:
+			export_table_to_csv_chunked(con, table, CHUNK_SIZE)
 
-		log("Writing to CSV file...")
-		df.to_csv(f"/data/{table}.csv", index=False)
-		log("Done!\n")
+		log(f"Done with {table}!\n")
+		log("-"*10)
 
-	cur.close()
 	con.close()
 	return
 
